cff7ef4b66cd4b2ed2d00fd347e3f090e1273914
李晋江 <lijinjiang01@126.com>
2023-06-08 14:44:04 +0800

    Initial commit

---
+++ src/main/java/com/lijinjiang/layui/Application.java
@@ -0,0 +1,13 @@
+package com.lijinjiang.layui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayuiModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayuiModuleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lijinjiang/layui/controller/LayuiController.java
@@ -0,0 +1,14 @@
+package com.lijinjiang.layui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 跳转页面控制器
+ * @Author 03010430
+ * @ModifyDate 2023/6/8 13:06
+ */
+@RestController
+@RequestMapping("/layui")
+public class BaseController {
+}
