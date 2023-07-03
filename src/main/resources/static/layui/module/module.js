a4c980286f5ea53b7388eb6c29f217782afeb8d4
李晋江 <lijinjiang01@126.com>
2023-07-03 14:45:41 +0800

    更新到最新版，实用let替代var

--- src/main/resources/static/layui/module/module.js
+++ src/main/resources/static/layui/module/module.js
@@ -1,5 +1,14 @@
+/*Layui Module 的路径*/
+window.modulePath = (function (src) {
+    src = document.currentScript
+        ? document.currentScript.src
+        : document.scripts[document.scripts.length - 1].src;
+    return src.substring(0, src.lastIndexOf("/") + 1);
+})();
+
 layui.config({
-    base: './module/'
+    base: modulePath,
+    version: "2.8.4"
 }).extend({
     iconPicker: 'iconPicker/iconPicker'
 });
