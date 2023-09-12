8be15b839c84b38e4c24bd6ad96186ccfdd0168d
LiJinJiang <lijinjiang01@126.com>
2023-09-12 14:29:02 +0800

    添加用户类

---
+++ src/main/java/com/lijinjiang/layui/entity/vo/User.java
@@ -0,0 +1,16 @@
+package com.lijinjiang.layui.entity.vo;
+
+import lombok.Data;
+
+/**
+ * @Description 用户类
+ * @Author 03010430
+ * @ModifyDate 2023/9/12 14:20
+ */
+@Data
+public class User {
+    private String id;
+    private String name;
+    private String address;
+    private String phone;
+}
