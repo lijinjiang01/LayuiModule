fd7dd4914d56d8d8b5ec62e93efd169532e6386f
LiJinJiang <lijinjiang01@126.com>
2023-09-15 17:18:03 +0800

    将iconPicker改为layui的内置模块

--- src/main/resources/static/layui/module/module.js
+++ src/main/resources/static/layui/module/module.js
@@ -1,14 +1,4 @@
-/*Layui Module 的路径*/
-window.modulePath = (function (src) {
-    src = document.currentScript
-        ? document.currentScript.src
-        : document.scripts[document.scripts.length - 1].src;
-    return src.substring(0, src.lastIndexOf("/") + 1);
-})();
-
 layui.config({
-    base: modulePath,
     version: "2.8.4"
 }).extend({
-    iconPicker: 'iconPicker/iconPicker'
 });
